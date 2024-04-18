7e3620b033bc737f87a0728ea7ea7f34ba85efbf
Bayarjargal <jargal.otgon@yahoo.com>
2024-04-18 09:52:24 +0800

    first

---
+++ example/views.py
@@ -0,0 +1,27 @@
+# example/views.py
+from datetime import datetime
+
+# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+def index(request):
+    return render(request, 'index.html')
+def trainer(request):
+    return render(request, 'trainer.html')
+def signup(request):
+    return render(request, 'signup.html')
+def why(request):
+    return render(request, 'why.html')
+def contact(request):
+    return render(request, 'contact.html')
+
+
+
+# from django.urls import path
+# from . import views
+
+
+
+# urlpatterns = [
+    
+# ]
